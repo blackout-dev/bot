83a7fadbd854b339f55e0cc64ebcea196419d1ce
Jonah Snider <jonah@jonah.pw>
2020-05-28 02:10:49 -0700

    refactor: change presence activity to 'your bots'
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/util/client-options.ts
+++ src/util/client-options.ts
@@ -2,7 +2,7 @@ import {ClientOptions, Intents} from 'discord.js';
 
 export const clientOptions: ClientOptions = {
 	disableMentions: 'everyone',
-	presence: {activity: {name: 'you', type: 'WATCHING'}},
+	presence: {activity: {name: 'your bots', type: 'WATCHING'}},
 	ws: {intents: [Intents.FLAGS.GUILD_PRESENCES]},
 	fetchAllMembers: true
 };
