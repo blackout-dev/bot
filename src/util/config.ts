84f344de1bc4fa833c05440c3a30217bef4202ce
Jonah Snider <jonah@jonah.pw>
2020-05-28 02:15:59 -0700

    refactor: remove unecessary logging from config
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,10 +1,7 @@
 import * as dotenv from 'dotenv';
 import {join as joinPaths} from 'path';
-import {logger} from './logger';
 
 const path = joinPaths(__dirname, '..', '..', 'bot.env');
-
-logger.info(path);
 
 if (process.env.NODE_ENV !== 'production') {
 	dotenv.config({path});
